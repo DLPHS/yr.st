43a30cf04fa56404eec044b74032ab9c6a509430
morgan-karmetik <jake.morgan@karmetik.com>
2026-02-17 02:09:14 -0800

    transparancy changes

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -3,7 +3,7 @@ const isMobile = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) ||
 
 if (!isMobile) {
   function setCursorSize() {
-    const size = Math.max(150, Math.min(window.innerWidth, window.innerHeight) * 0.25);
+    const size = Math.max(123, Math.min(window.innerWidth, window.innerHeight) * 0.21);
     cursor.style.width = size + 'px';
     cursor.style.height = size + 'px';
   }
